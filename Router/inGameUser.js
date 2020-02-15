6fd238d07e5d465a021c7eea3ea0b468e4ef59bc
Kent Roth <kentencroth@yahoo.com>
2020-02-15 08:52:18 -0600

    In game user get and patch by id

--- Router/inGameUser.js
+++ Router/inGameUser.js
@@ -22,4 +22,43 @@ router.get('/inGameUser', async (req, res) => {
 	}
 });
 
+router.get('/inGameUser/:id', async (req, res) => {
+	const _id = req.params.id;
+	try {
+		const inGameUser = await InGameUser.findById({ _id });
+		if (!inGameUser) {
+			return res.status(400).send();
+		}
+		res.send(inGameUser);
+	} catch (error) {
+		res.status(500).send();
+	}
+});
+
+router.patch('/inGameUser/:id', async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ['name', 'property', 'money', 'netWorth'];
+	const isValidUpdate = updates.every(update => {
+		allowedUpdates.includes(update);
+	});
+	const _id = req.params.id;
+
+	if (!isValidUpdate) {
+		return res.status(400).send({ error: 'Update Failed' });
+	}
+
+	try {
+		const inGameUser = await InGameUser.findByIdAndUpdate(_id, req.body, {
+			new: true,
+			runValidators: true
+		});
+		if (!inGameUser) {
+			res.status(404).send();
+		}
+		res.send(inGameUser);
+	} catch (error) {
+		res.status(500).send();
+	}
+});
+
 module.exports = router;
